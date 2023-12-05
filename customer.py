61c7f050f3ade78f7e204d713385c7dab437e5f0
TradingCyborg <my.github.kinyuad29@gmail.com>
2023-12-05 10:31:34 +0300

    commits

---
+++ customer.py
@@ -0,0 +1,21 @@
+class Customer:
+    all_customers = []
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        Customer.all_customers.append(self)
+    def set_given_name(self, new_given_name):
+        self.given_name = new_given_name
+    def set_family_name(self, new_family_name):
+        self.family_name = new_family_name
+    def given_name(self):
+        return self.given_name
+    def family_name(self):
+        return self.family_name
+    def full_name(self):
+        return f"{self.given_name} {self.family_name}"
+    @classmethod
+    def all(cls):
+        return cls.all_customers
+    
---
+++ restaurant.py
@@ -0,0 +1,12 @@
+from customer import Customer
+class Restaurant:
+    def __init__(self, name):
+        self.name = name
+    def name(self):
+        return self-__name
+    # Example usgae:
+    restaurant = Restaurant("Tasty Delights")
+
+    #Accessing the restaurant's name
+    print(f"Restaurant's name: {restaurant.name()}")
+    
